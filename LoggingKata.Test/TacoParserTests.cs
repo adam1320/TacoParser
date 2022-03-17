1070406310e2c98e6f801cef60fba9ba48e94832
Adam Brannon <adam1320@rocketmail.com>
2022-03-17 07:03:49 -0400

    Cleaned up project to make it presentable

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -9,38 +9,26 @@ namespace LoggingKata.Test
         
         public void ShouldDoSomething()
         {
-            // TODO: Complete Something, if anything
 
-            //Arrange
             var tacoParser = new TacoParser();
 
-            //Act
             var actual = tacoParser.Parse("34.073638, -84.677017, Taco Bell Acwort...");
 
-            //Assert
             Assert.NotNull(actual);
-
         }
 
         [Theory]
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
         public void ShouldParseLongitude(string line, double expected)
         {
-            // TODO: Complete - "line" represents input data we will Parse to
-            //       extract the Longitude.  Your .csv file will have many of these lines,
-            //       each representing a TacoBell location
-
-            //Arrange
             var test = new TacoParser();
-            //Act
+           
             var actual = test.Parse(line).Location.Longitude;
 
-            //Assert
             Assert.Equal(expected, actual);
         }
 
 
-        //TODO: Create a test ShouldParseLatitude
         [Theory]
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
         public void ShouldParseLatitude(string line, double expected)
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,8 +1,6 @@
 ﻿namespace LoggingKata
 {
-    /// <summary>
-    /// Parses a POI file to locate all the Taco Bells
-    /// </summary>
+
     public class TacoParser
     {
         readonly ILog logger = new TacoLogger();
@@ -11,42 +9,28 @@
         {
             logger.LogInfo("Begin parsing");
 
-            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
             var cells = line.Split(',');
-
-            // If your array.Length is less than 3, something went wrong
+           
             if (cells.Length < 3)
             {
-                // Log that and return null
+                
                 logger.LogWarning("Incomplete data input, less than 3 items.");
-                // Do not fail if one record parsing fails, return null
-                return null; // TODO Implement
+                
+                return null; 
             }
 
-            // grab the latitude from your array at index 0
             var latitude = double.Parse(cells[0]);
-            // grab the longitude from your array at index 1
+            
             var longitude = double.Parse(cells[1]);
-            // grab the name from your array at index 2
+            
             string name = cells[2];
 
-            // DONE Your going to need to parse your string as a `double`
-            // DONE which is similar to parsing a string as an `int`
-
-            // DONE You'll need to create a TacoBell class
-            // DONE that conforms to ITrackable
-
-            // Then, you'll need an instance of the TacoBell class
-            // With the name and point set correctly
             var point = new Point();
             point.Latitude = latitude;
             point.Longitude = longitude;
             var tacoBell = new TacoBell();
             tacoBell.Name = name;
             tacoBell.Location = point;
-
-            // Then, return the instance of your TacoBell class
-            // Since it conforms to ITrackable
 
             return tacoBell;
         }
